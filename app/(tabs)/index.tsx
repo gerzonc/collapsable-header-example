30ebd386280a53581d37092c5d21340e75698958
Gerzon Zorrilla <gerzonc@icloud.com>
2024-08-30 17:40:55 -0400

    chore: re-organize code and improve header animation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,5 @@
-import mockData from "@/sample/mockData";
-import {
-  View,
-  Text,
-  StyleSheet,
-  type ListRenderItem,
-  useWindowDimensions
-} from "react-native";
+import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
+import type { ListRenderItem, FlatListProps } from "react-native";
 import Animated, {
   Extrapolation,
   interpolate,
@@ -14,82 +8,71 @@ import Animated, {
   useSharedValue
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import mockData from "@/sample/mockData";
 
-const EXPANDED_HEIGHT = 220;
-const COLLAPSED_HEIGHT = 144;
+const EXPANDED_HEIGHT = 200;
+const COLLAPSED_HEIGHT = 96;
+
+interface Item {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+}
+
+interface ListProps extends FlatListProps<Item> {
+  data: Item[];
+  renderItem: ListRenderItem<Item>;
+  height: number;
+}
+
+interface HeaderProps {
+  top: number;
+  animatedTextStyleOne: {
+    transform: {
+      translateY: number;
+    }[];
+  };
+  animatedHeaderStyle: {
+    height: number;
+  };
+}
 
 export default function HomeScreen() {
   const translateY = useSharedValue(0);
   const { top } = useSafeAreaInsets();
   const { height } = useWindowDimensions();
-  const textWidth = useSharedValue(0);
+
+  const animatedHeaderStyle = useAnimatedStyle(() => ({
+    height: interpolate(
+      translateY.value,
+      [0, 120],
+      [EXPANDED_HEIGHT, COLLAPSED_HEIGHT],
+      Extrapolation.CLAMP
+    )
+  }));
+
+  const animatedTextStyleOne = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: interpolate(
+          translateY.value,
+          [0, COLLAPSED_HEIGHT],
+          [COLLAPSED_HEIGHT, 0],
+          Extrapolation.CLAMP
+        )
+      }
+    ]
+  }));
+
   const scrollHandler = useAnimatedScrollHandler({
     onScroll: (event) => {
       "worklet";
       translateY.value = event.contentOffset.y;
     }
   });
-  const animatedHeaderStyle = useAnimatedStyle(() => {
-    return {
-      height: interpolate(
-        translateY.value,
-        [0, COLLAPSED_HEIGHT],
-        [EXPANDED_HEIGHT, COLLAPSED_HEIGHT],
-        Extrapolation.CLAMP
-      )
-    };
-  });
 
-  const animatedTextStyleOne = useAnimatedStyle(() => {
-    const translation = interpolate(
-      translateY.value,
-      [0, 45],
-      [0, -100],
-      Extrapolation.CLAMP
-    );
-    const translateX = interpolate(
-      translateY.value,
-      [0, 30],
-      [0, -100],
-      Extrapolation.CLAMP
-    );
-    const scale = interpolate(
-      translateY.value,
-      [0, 45],
-      [1, 0],
-      Extrapolation.CLAMP
-    );
-    return {
-      opacity: interpolate(translateY.value, [0, 30], [1, 0]),
-      transform: [{ translateY: translation }, { translateX }, { scale }]
-    };
-  });
-
-  const animatedTextStyleTwo = useAnimatedStyle(() => {
-    const translation = interpolate(
-      translateY.value,
-      [0, 120],
-      [15, 0],
-      Extrapolation.CLAMP
-    );
-    const scale = interpolate(
-      translateY.value,
-      [0, 120],
-      [2, 1],
-      Extrapolation.CLAMP
-    );
-    return {
-      transform: [{ translateY: translation }, { scale }],
-      opacity: interpolate(translateY.value, [0, 45], [0, 1])
-    };
-  });
-
-  const renderItem: ListRenderItem<{
-    id: string;
-    title: string;
-    description: string;
-    date: string;
-  }> = ({ item }) => (
+  const renderItem: ListRenderItem<Item> = ({ item }) => (
     <View style={styles.itemContainer}>
       <Text style={styles.title}>{item.title}</Text>
       <Text style={styles.description}>{item.description}</Text>
@@ -99,58 +82,77 @@ export default function HomeScreen() {
 
   return (
     <View>
-      <Animated.View
-        style={[
-          {
-            paddingTop: top,
-            backgroundColor: "red",
-            justifyContent: "flex-end",
-            padding: 16
-          },
-          animatedHeaderStyle
-        ]}
-      >
-        <Animated.Text
-          style={[
-            {
-              fontSize: 25,
-              alignSelf: "center"
-            },
-            animatedTextStyleTwo
-          ]}
-        >
-          Hello World
-        </Animated.Text>
-        <Animated.Text
-          adjustsFontSizeToFit
-          style={[{ fontSize: 32 }, animatedTextStyleOne]}
-          onLayout={({ nativeEvent }) => {
-            textWidth.value = nativeEvent.layout.width;
-          }}
-        >
-          Hello World
-        </Animated.Text>
-      </Animated.View>
-      <Animated.FlatList
+      <AnimatedHeader
+        top={top}
+        animatedTextStyleOne={animatedTextStyleOne}
+        animatedHeaderStyle={animatedHeaderStyle}
+      />
+      <AnimatedFlatList
         data={mockData}
-        onScroll={scrollHandler}
         renderItem={renderItem}
-        snapToAlignment="start"
-        snapToInterval={height / 5}
+        onScroll={scrollHandler}
+        height={height}
         scrollEventThrottle={16}
-        automaticallyAdjustsScrollIndicatorInsets
-        automaticallyAdjustContentInsets
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.list}
       />
     </View>
   );
 }
 
+const AnimatedHeader = ({
+  top,
+  animatedTextStyleOne,
+  animatedHeaderStyle
+}: HeaderProps) => (
+  <>
+    <Animated.View
+      style={[styles.collapsedHeader, { paddingTop: COLLAPSED_HEIGHT }]}
+    >
+      <Animated.Text
+        style={[styles.collapsedHeaderText, { top }, animatedTextStyleOne]}
+      >
+        Hello World
+      </Animated.Text>
+    </Animated.View>
+    <Animated.View style={[styles.expandedHeader, animatedHeaderStyle]}>
+      <Animated.View style={styles.expandedHeaderContent}>
+        <Animated.Text style={styles.expandedHeaderText}>
+          Hello World
+        </Animated.Text>
+      </Animated.View>
+    </Animated.View>
+  </>
+);
+
+const AnimatedFlatList = ({
+  data,
+  renderItem,
+  onScroll,
+  height,
+  scrollEventThrottle
+}: ListProps) => (
+  <Animated.FlatList
+    data={data}
+    renderItem={renderItem}
+    onScroll={onScroll}
+    snapToAlignment="start"
+    snapToInterval={height / 5}
+    scrollEventThrottle={scrollEventThrottle}
+    automaticallyAdjustsScrollIndicatorInsets
+    automaticallyAdjustContentInsets
+    keyExtractor={(item) => item.id}
+    contentContainerStyle={styles.list}
+    scrollIndicatorInsets={{
+      top: EXPANDED_HEIGHT - 70,
+      bottom: EXPANDED_HEIGHT - 70
+    }}
+  />
+);
+
 const styles = StyleSheet.create({
   list: {
     padding: 16,
-    paddingBottom: EXPANDED_HEIGHT
+    paddingBottom: EXPANDED_HEIGHT,
+    paddingTop: EXPANDED_HEIGHT / 1.5
   },
   itemContainer: {
     backgroundColor: "#f9f9f9",
@@ -175,5 +177,31 @@ const styles = StyleSheet.create({
   date: {
     fontSize: 12,
     color: "#aaa"
+  },
+  collapsedHeader: {
+    backgroundColor: "red",
+    overflow: "hidden",
+    zIndex: 3
+  },
+  collapsedHeaderText: {
+    ...StyleSheet.absoluteFillObject,
+    textAlign: "center",
+    position: "absolute",
+    fontSize: 22,
+    zIndex: 2
+  },
+  expandedHeader: {
+    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    backgroundColor: "red",
+    padding: 16,
+    zIndex: 2
+  },
+  expandedHeaderContent: {
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  expandedHeaderText: {
+    fontSize: 32
   }
 });
